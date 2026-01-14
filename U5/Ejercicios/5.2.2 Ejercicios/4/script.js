1536f77c3abd10c15ac4156c4f5f16bf56569ec9
Robert <robert.sebastian.visu@gmail.com>
2026-01-14 13:04:53 +0100

    Clase 14/01/2026

---
+++ U5/Ejercicios/5.2.2 Ejercicios/4/script.js
@@ -0,0 +1,30 @@
+// Main
+document.addEventListener("DOMContentLoaded", function () {
+    /*
+    // Divs
+    let div_arriba_izquierda = document.getElementById("arriba_izquierda");
+    let div_arriba_derecha = document.getElementById("arriba_derecha");
+    let div_abajo_izquierda = document.getElementById("abajo_izquierda");
+    let div_abajo_derecha = document.getElementById("abajo_derecha");
+
+    // Listeners
+    div_arriba_izquierda.addEventListener("click", () => alert("Has clickado arriba a la izquierda."));
+    div_arriba_derecha.addEventListener("click", () => alert("Has clickado arriba a la derecha."));
+    div_abajo_izquierda.addEventListener("click", () => alert("Has clickado abajo a la izquierda."));
+    div_abajo_derecha.addEventListener("click", () => alert("Has clickado abajo a la derecha."));
+    */
+    
+    // Recuperamos todos los cuadrantes
+    let divs = document.getElementsByName("cuadrante");
+    
+    // Iteramos los cuadrantes (divs)
+    for (const div of divs) {
+        // Por cada cuadrante le adjuntamos un event listener de click
+        div.addEventListener("click", function () {
+            // Creamos el mensaje segun la id y ejecutamos un alert
+            let posicion = div.id.replace("_", " ");
+            let mensaje = `Has hecho click en el cuadrante "${posicion}"`;
+            alert(mensaje);
+        });
+    }
+});
